0324256de50948ef4397a1fa1cc85ce684a1b395
Kacprucha <karas.kacper@gmail.com>
2023-10-18 08:56:17 +0200

    Implementing OpenAPI3

--- src/main/java/com/example/backened/controllers/NoteController.java
+++ src/main/java/com/example/backened/controllers/NoteController.java
@@ -2,6 +2,12 @@ package com.example.backened.controllers;
 
 import com.example.backened.entities.Note;
 import com.example.backened.services.NoteService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.Collection;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -18,12 +24,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
+@Tag(name = "Notes", description = "Notes management APIs")
 @RequestMapping(value = "notes")
 @RequiredArgsConstructor
 public class NoteController {
 
   private final NoteService noteService;
 
+  @Operation(
+      summary = "Create a Note",
+      description = "Create a Note object.",
+      tags = {"post"})
   @PostMapping
   @ResponseStatus(HttpStatus.CREATED)
   Note create (@RequestBody Note note) 
@@ -32,6 +43,10 @@ public class NoteController {
     return noteService.save (note);
   }
 
+  @Operation(
+      summary = "Find all Notes",
+      description = "Get all Note objects.",
+      tags = {"get"})
   @GetMapping
   Collection<Note> findAll () 
   {
@@ -39,8 +54,24 @@ public class NoteController {
     return noteService.findAll ();
   }
 
+  @Operation(
+    summary = "Find a Note by Id",
+    description = "Get a Note object by specifying its id.",
+    tags = {"get"})
+  @ApiResponse(
+    responseCode = "200",
+    content = {
+      @Content(schema = @Schema(implementation = Note.class), mediaType = "application/json")})
+  @ApiResponse(
+    responseCode = "404",
+    content = {@Content(schema = @Schema())})
+  @ApiResponse(
+    responseCode = "500",
+    content = {@Content(schema = @Schema())})
   @GetMapping("/{id}")
-  Note findById(@PathVariable Long id) 
+  Note findById (
+    @Parameter(description = "Note Id.", example = "1")
+    @PathVariable Long id)
   {
     log.debug ("Find note with id: {}", id);
     return noteService.findById (id);
@@ -53,8 +84,14 @@ public class NoteController {
     return noteService.update (id, note);
   }
 
+  @Operation(
+      summary = "Delete a Note by Id",
+      description = "Delete a Note object by specifying its id.",
+      tags = {"delete"})
   @DeleteMapping("/{id}")
-  void deleteEmployee (@PathVariable Long id) 
+  void deleteEmployee (
+    @Parameter(description = "Note Id.", example = "1")
+    @PathVariable Long id) 
   {
     log.debug ("Delete note with id: {}", id);
     noteService.deleteById (id);
