6dcc2d5c217e23a9e52a5a64c967db71c3fc6468
Kacprucha <karas.kacper@gmail.com>
2023-12-05 09:41:46 +0100

    Configure audit on bacend

--- backend/src/main/java/com/example/backend/configuration/AuditConfig.java
+++ backend/src/main/java/com/example/backend/configuration/AuditConfig.java
@@ -1,23 +1,13 @@
 package com.example.backend.configuration;
 
+import java.util.Optional;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.domain.AuditorAware;
-import static org.springframework.security.config.Customizer.withDefaults;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.stereotype.Component;
-
-import java.util.Optional;
 
 
 @Configuration
@@ -25,43 +15,11 @@ import java.util.Optional;
 public class AuditConfig 
 {
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-            .csrf(CsrfConfigurer::disable)
-            .authorizeHttpRequests((authorize) -> authorize
-                // Allow access to Swagger
-                .requestMatchers(
-                    "/v3/api-docs/**",
-                    "/swagger-ui/**",
-                    "/swagger-ui.html",
-                    "/h2-console/**"
-                ).permitAll()
-                // Authenticate all other requests
-                .anyRequest().authenticated()
-            )
-            // Use basic authentication (user/pass)
-            .formLogin(withDefaults());
-
-        return http.build();
+    public AuditorAware<String> auditorProvider() 
+    {
+        return () -> Optional.ofNullable(SecurityContextHolder.getContext())
+            .map(SecurityContext::getAuthentication)
+            .map(Authentication::getName)
+            .or(() -> Optional.of("anonymous"));
     }
-
-    @Bean
-    public AuditorAware<String> auditorProvider() {
-        return () -> Optional.of(SecurityContextHolder.getContext())
-        .map(v -> v.getAuthentication())
-        .map(v -> v.getName());
-    }
-
-    @Bean
-	public UserDetailsService userDetailsService() {
-		UserDetails user =
-			 User.withDefaultPasswordEncoder()
-				.username("user")
-				.password("password")
-				.roles("USER")
-				.build();
-
-		return new InMemoryUserDetailsManager(user);
-	}
-
 }
