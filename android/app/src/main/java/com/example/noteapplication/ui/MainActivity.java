78ba1db60fdc7d8412c8b4f7958e3d208390aebc
Kacprucha <karas.kacper@gmail.com>
2023-12-28 18:53:23 +0100

    Forgoten files

---
+++ android/app/src/main/java/com/example/noteapplication/ui/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.noteapplication.ui;
+
+import android.os.Bundle;
+import android.util.Log;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import com.example.noteapplication.R;
+import com.example.noteapplication.adapters.NoteAdapter;
+import com.example.noteapplication.model.Note;
+import com.example.noteapplication.viewmodel.NoteViewModel;
+import dagger.hilt.android.AndroidEntryPoint;
+import java.util.ArrayList;
+import java.util.List;
+@AndroidEntryPoint
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    private List<Note> notes = new ArrayList<>();
+    private RecyclerView recyclerView;
+    private NoteViewModel viewModel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        viewModel = new ViewModelProvider(this).get(NoteViewModel.class);
+
+        initRecyclerView();
+        observeData();
+        viewModel.fetchNotes();
+    }
+
+    private void initRecyclerView() {
+        recyclerView = findViewById(R.id.list);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(new NoteAdapter(notes));
+    }
+
+    private void observeData() {
+        viewModel.getNoteList().observe(this, newNotes -> {
+            Log.e(TAG, "onChanged: " + newNotes.size());
+            notes = newNotes;
+            recyclerView.setAdapter(new NoteAdapter(notes));
+        });
+    }
+}
